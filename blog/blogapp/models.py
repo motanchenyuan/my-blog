f2e1ffc436c42e12342b5fa3f9da3223f26ac3d3
yannian <motanchenyuan@126.com>
2018-10-18 17:05:36 +0800

    2018.10.18

--- blog/blogapp/models.py
+++ blog/blogapp/models.py
@@ -1,9 +1,9 @@
 # encoding:utf8
 from django.db import models
 from django.contrib import admin
+from django.contrib.auth.models import User
 
-
-class Article(models.Model):
+'''class Article(models.Model):
     title = models.CharField(max_length=100)    
     category = models.TextField(max_length=50, blank=True)  
     date_time = models.DateTimeField(auto_now_add=True) 
@@ -17,3 +17,24 @@ class Article(models.Model):
 
 # Register model
 admin.site.register(Article)
+'''
+class Category(models.Model):
+	name = models.CharField(max_length=100)
+	def __str__(self):
+		return self.name
+class Tag(models.Model):
+	name = models.CharField(max_length=100)
+	def __str__(self):                 
+		return self.name
+class Post(models.Model):
+	title = models.CharField(max_length=70)
+	def __str__(self):                 
+		return self.name
+	body = models.TextField()
+	create_time = models.DateTimeField()
+	modeified_time = models.DateTimeField()
+	excerpt = models.CharField(max_length= 200,blank = True)
+	category = models.ForeignKey(Category, on_delete=models.CASCADE)
+	tags = models.ManyToManyField(Tag,blank=True)
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	
--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Post
+def index(request):
+	post_list = Post.objects.all().order_by('-create_time')
+	return render(request,'blog/index.html',context={
+		'title':'welcom to my blog',
+		'welcome':'welcom to my blog index',
+		'post_list':post_list,
+
+	})
