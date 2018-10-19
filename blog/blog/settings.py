2a88a19f1c9ad0f7a831930d059759944cd65b9e
motanchenyuan <37241961+motanchenyuan@users.noreply.github.com>
2018-10-19 14:28:40 +0800

    Update settings.py

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2)oof0l$43gc)ldsv8l6_j+68ydpz20nqfp)cwp6l#^m9(5zog'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.227.166.152','127.0.0.1','0.0.0.0']
+ALLOWED_HOSTS = ['192.227.166.152','127.0.0.1','0.0.0.0','mtcy.site']
 
 
 # Application definition
