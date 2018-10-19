148d3efeadb27f38ed273942fbde96d42150acb6
yannian <motanchenyuan@126.com>
2018-10-19 09:35:53 +0800

    fix static link problems

--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -4,6 +4,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Post
+
+'''from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns += staticfiles_urlpatterns()
+'''
 def index(request):
 	post_list = Post.objects.all().order_by('-create_time')
 	return render(request,'blog/index.html',context={
