904dcc318d69cf44f75cfb3f9c5c1ddaff0d0721
motanchenyuan <37241961+motanchenyuan@users.noreply.github.com>
2018-11-15 13:56:18 +0800

    Update forms.py

--- blog/comments/forms.py
+++ blog/comments/forms.py
@@ -1,8 +1,7 @@
 from django import forms
 from .models import Comment
 
-
 class CommentForm(forms.ModelForm):
     class Meta:
-            model = Comment
-	            fields = ['name', 'email', 'url', 'text']
+    	model = Comment
+	fields = ['name', 'email', 'url', 'text']
