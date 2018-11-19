6c8fcf4c4cd43a18aaf5e18c077cad88b88e6fad
motanchenyuan <37241961+motanchenyuan@users.noreply.github.com>
2018-11-19 11:39:16 +0800

    Update models.py

--- blog/blogapp/models.py
+++ blog/blogapp/models.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 from django.utils.six import python_2_unicode_compatible
 #from django.contrib.auth.models import AbstractUser
 
-
+'''
 class UserInfo(AbstractUser):
     """
     用户信息
@@ -28,7 +28,7 @@ class Blog(models.Model):
     theme = models.CharField(verbose_name='博客主题', max_length=32)
     def __str__(self):
         return self.title
-
+'''
 '''class Article(models.Model):
     title = models.CharField(max_length=100)    
     category = models.TextField(max_length=50, blank=True)  
