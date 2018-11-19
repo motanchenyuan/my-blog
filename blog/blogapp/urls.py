546e11ce6d81058ff94beab276ebf7163a3042aa
motanchenyuan <37241961+motanchenyuan@users.noreply.github.com>
2018-11-19 11:23:04 +0800

    Update urls.py

--- blog/blogapp/urls.py
+++ blog/blogapp/urls.py
@@ -9,6 +9,5 @@ urlpatterns = [
 	url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.archives, name='archives'),
 	url(r'^category/(?P<pk>[0-9]+)/$', views.category, name='category'),
 	url(r'^register/$',views.register, name='register'),
-        url(r'^login/$',views.my_login, name='my_login'),
-        url(r'^logout/$',views.my_logout, name='my_logout'),
+        
 ]
