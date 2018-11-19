f1d3f94da99b875913b9aed338eaaaeecba76785
motanchenyuan <37241961+motanchenyuan@users.noreply.github.com>
2018-11-19 11:07:23 +0800

    Update forms.py

--- blog/comments/forms.py
+++ blog/comments/forms.py
@@ -1,7 +1,15 @@
 from django import forms
 from .models import Comment
+#以下为用户注册导入
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
 
 class CommentForm(forms.ModelForm):
 	class Meta:
 		model = Comment
 		fields = ['name','email','text']
+#注册部分代码
+class RegisterForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ("username", "email")
