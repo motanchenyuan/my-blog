4f72f41051c5e4aef85621b76abddb32c81ca149
motanchenyuan <37241961+motanchenyuan@users.noreply.github.com>
2018-12-06 11:07:54 +0800

    Update models.py

--- blog/blogapp/models.py
+++ blog/blogapp/models.py
@@ -69,7 +69,7 @@ class Post(models.Model):
 	excerpt = models.CharField(max_length= 200,blank = True)
 	category = models.ForeignKey(Category, on_delete=models.CASCADE)
 	tags = models.ManyToManyField(Tag,blank=True)
-	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 	def __str__(self):                 
 		return self.title
 	def get_absolute_url(self):
