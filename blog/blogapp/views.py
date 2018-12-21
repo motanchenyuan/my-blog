e8a9868f9ff97f99571e74e79be0bd0dff0c1f41
motanchenyuan <37241961+motanchenyuan@users.noreply.github.com>
2018-12-21 20:06:26 +0800

    Update views.py

--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -1,6 +1,9 @@
 import markdown
 from django.shortcuts import render
 from comments.forms import CommentForm
+
+
+from django.views.generic import ListView, DetailView
 #注册登录
 #from django.shortcuts import render, redirect
 #from comments.forms import RegisterForm
@@ -12,7 +15,9 @@ from .models import Post,Category
 from django.shortcuts import render, get_object_or_404
 #设置分页
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 #以下为分页
+
 class IndexView(ListView):
     model = Post
     template_name = 'blogapp/index.html'
@@ -20,6 +25,58 @@ class IndexView(ListView):
     # 指定 paginate_by 属性后开启分页功能，其值代表每一页包含多少篇文章
     paginate_by = 10
 
+class CategoryView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+
+    def get_queryset(self):
+        cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
+        return super(CategoryView, self).get_queryset().filter(category=cate)
+
+
+class PostDetailView(DetailView):
+    # 这些属性的含义和 ListView 是一样的
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+
+    def get(self, request, *args, **kwargs):
+        # 覆写 get 方法的目的是因为每当文章被访问一次，就得将文章阅读量 +1
+        # get 方法返回的是一个 HttpResponse 实例
+        # 之所以需要先调用父类的 get 方法，是因为只有当 get 方法被调用后，
+        # 才有 self.object 属性，其值为 Post 模型实例，即被访问的文章 post
+        response = super(PostDetailView, self).get(request, *args, **kwargs)
+
+        # 将文章阅读量 +1
+        # 注意 self.object 的值就是被访问的文章 post
+        self.object.increase_views()
+
+        # 视图必须返回一个 HttpResponse 对象
+        return response
+
+    def get_object(self, queryset=None):
+        # 覆写 get_object 方法的目的是因为需要对 post 的 body 值进行渲染
+        post = super(PostDetailView, self).get_object(queryset=None)
+        post.body = markdown.markdown(post.body,
+                                      extensions=[
+                                          'markdown.extensions.extra',
+                                          'markdown.extensions.codehilite',
+                                          'markdown.extensions.toc',
+                                      ])
+        return post
+
+    def get_context_data(self, **kwargs):
+        # 覆写 get_context_data 的目的是因为除了将 post 传递给模板外（DetailView 已经帮我们完成），
+        # 还要把评论表单、post 下的评论列表传递给模板。
+        context = super(PostDetailView, self).get_context_data(**kwargs)
+        form = CommentForm()
+        comment_list = self.object.comment_set.all()
+        context.update({
+            'form': form,
+            'comment_list': comment_list
+        })
+        return context
 
 
 '''from django.contrib.staticfiles.urls import staticfiles_urlpatterns
