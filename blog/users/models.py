236661c31b20614ad5f49d6e858566e7ab8160f5
motanchenyuan <37241961+motanchenyuan@users.noreply.github.com>
2018-12-21 22:32:58 +0800

    Update models.py

--- blog/users/models.py
+++ blog/users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     nickname = models.CharField(max_length=50, blank=True)
-     list_display = ("Email address，")
+    list_display = ("Email address，")
     class Meta(AbstractUser.Meta):
             pass
 
