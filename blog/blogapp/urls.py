cf9c5b3840ea98a86c7d2b7be9da566f79db3db7
motanchenyuan <37241961+motanchenyuan@users.noreply.github.com>
2018-12-21 20:09:10 +0800

    Update urls.py

--- blog/blogapp/urls.py
+++ blog/blogapp/urls.py
@@ -2,12 +2,12 @@ from django.conf.urls import url
 from . import views
 app_name = 'blogapp'
 urlpatterns = [
-	url(r'^$',views.index,name='index'),
+	url(r'^$',views.IndexView.as_view(),name='index'),
 	url(r'^index.html/',views.index,name='index'),
 	url(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
 #	url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$, views.archives, name='archives'),
 	url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.archives, name='archives'),
-	url(r'^category/(?P<pk>[0-9]+)/$', views.category, name='category'),
+	url(r'^category/(?P<pk>[0-9]+)/$', views.CategoryView.as_view(), name='category'),
 	url(r'^tag/(?P<pk>[0-9]+)/$',views.tag, name='tag'),
         
 ]
