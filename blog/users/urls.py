555b10feb60a2097669f611347533a45d15dcd08
motanchenyuan <37241961+motanchenyuan@users.noreply.github.com>
2018-12-22 22:52:06 +0800

    Update urls.py

--- blog/users/urls.py
+++ blog/users/urls.py
@@ -7,10 +7,5 @@ from users import views
 app_name = 'users'
 urlpatterns = [
     url(r'^register/', views.register, name='register'),
-     url(r'^admin/', admin.site.urls),
-    # 别忘记在顶部引入 include 函数
-    url(r'^users/', include('users.urls')),
-    url(r'^users/', include('django.contrib.auth.urls')),
-    # 别忘记在顶部引入 views 模块
-    url(r'^$', views.index, name='index')
+     
         ]
